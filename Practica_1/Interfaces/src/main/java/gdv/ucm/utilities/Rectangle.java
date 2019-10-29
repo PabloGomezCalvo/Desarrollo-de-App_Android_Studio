e25c8ff78dbdd9a693aafc5cbb9a96c067803579
sergiojh <chequilin3@gmail.com>
2019-10-29 19:11:27 +0100

    Complete Sprite class and Graphics methods

---
+++ Practica_1/Interfaces/src/main/java/gdv/ucm/utilities/Rectangle.java
@@ -0,0 +1,9 @@
+package gdv.ucm.utilities;
+
+public class Rectangle {
+
+    public int _x;
+    public int _y;
+    public int _width;
+    public int _height;
+}
---
+++ Practica_1/Interfaces/src/main/java/gdv/ucm/utilities/Sprite.java
@@ -0,0 +1,18 @@
+package gdv.ucm.utilities;
+
+import gdv.ucm.interfaces.Graphics;
+import gdv.ucm.interfaces.Image;
+
+public class Sprite {
+
+    public Sprite(Image image, Rectangle rect){
+
+    }
+///tunearlo para q se pinte solo centrado y demas
+    public void draw(Graphics g, int destX, int destY){
+
+    }
+
+    private Image _img;
+    private Rectangle _rect;
+}
